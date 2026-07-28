6f20e254a5307a0efa78bad77e410fc471f8f28c
reiser02 <reiser976@outlook.es>
2026-07-28 23:43:10 +0200

    fix: silence optional Prophet plotting warning

--- src/airquality/__init__.py
+++ src/airquality/__init__.py
@@ -1,1 +1,9 @@
 """Airquality package."""
+
+import logging
+
+
+logging.getLogger("prophet.plot").addFilter(
+    lambda record: record.getMessage()
+    != "Importing plotly failed. Interactive plots will not work."
+)
